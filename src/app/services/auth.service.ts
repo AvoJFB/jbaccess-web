270cc3ea03c00f5a0478fb0d036ada3d7ac7bd5e
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-06-30 17:14:15 +0300

    Implement methods for auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
 
+  login(user: LoginInDto) {
+    return this.http.get('/security/login');
+  }
+
+  logout() {
+    return this.http.get('/security/logout');
+  }
+
+  restoreSession() {
+    return this.http.get('/security/restore-session');
+  }
 }
