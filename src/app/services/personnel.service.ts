4decd19de83be9526623c1da2f8bc41815a4275c
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-07-03 12:46:10 +0300

    Create PersonResponse model and refactor some model names

--- src/app/services/personnel.service.ts
+++ src/app/services/personnel.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { PersonResponse } from '../models/PersonResponse';
 
 @Injectable()
 export class PersonnelService {
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
 
+  getPersonById(id): Observable<PersonResponse> {
+    const params = new URLSearchParams();
+    params.set('id', id);
+
+    return this.http.get(`/person/${id}`)
+      .map(res => res.json());
+  }
 }
