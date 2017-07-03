6a8c3a818b4d8f1ff0b791f39fe7f306d766e454
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-07-03 19:01:51 +0300

    Use ng2-toastr for notifications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthService } from './services/auth.service';
 
 const appRoutes: Routes = [
   { path: '', component: DashboardComponent },
@@ -22,11 +25,16 @@ const appRoutes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
+    ToastModule.forRoot(),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewContainerRef } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
+import { LoginInDto } from '../../models/LoginInDto';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    public toastr: ToastsManager,
+    vcr: ViewContainerRef
+  ) {
+    this.toastr.setRootViewContainerRef(vcr);
   }
 
+  ngOnInit() {
+    this.loginForm = this.fb.group({
+      login: ['', [Validators.required, Validators.minLength(2)]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  showSuccess() {
+    this.toastr.success('You are awesome!', 'Success!');
+  }
+
+  onSubmit() {
+    if (!this.loginForm.get('login').valid) {
+      alert('login is not valid');
+    } else if (!this.loginForm.get('password').valid) {
+      alert('password is not valid');
+    } else {
+      const user: LoginInDto = {
+        login: this.loginForm.get('login').value,
+        password: this.loginForm.get('password').value
+      };
+      this.showSuccess();
+      console.log(`username: ${user.login}   password:${user.password}`);
+    }
+  }
 }
