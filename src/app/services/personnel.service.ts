07e2104221e0519034ee0fe80b5cd4b1315a129c
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-07-03 13:01:21 +0300

    Add updatePersonById method to personnel service

--- src/app/services/personnel.service.ts
+++ src/app/services/personnel.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { PersonResponse } from '../models/PersonResponse';
+import { PersonInDto } from '../models/PersonInDto';
 
 @Injectable()
 export class PersonnelService {
@@ -17,6 +18,14 @@ export class PersonnelService {
     params.set('id', id);
 
     return this.http.get(`/person/${id}`, { search: params })
+      .map(res => res.json());
+  }
+
+  updatePersonById(id, person: PersonInDto): Observable<PersonResponse> {
+    const params = new URLSearchParams();
+    params.set('id', id);
+
+    return this.http.put(`/person/${id}`, person)
       .map(res => res.json());
   }
 }
