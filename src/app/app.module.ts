8f551a15afa9bc1c9ff1f8f71fff49eb0ed684d1
Avetik Harutyunyan <avetik.harutyunyan1@gmail.com>
2017-07-03 15:01:06 +0300

    Generate dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
